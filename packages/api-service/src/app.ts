01c1ba5e4746b68f6fce34f992297a4ad6c71d49
Kevin Nemec <kevin.nemec@sony.com>
2023-12-07 15:49:52 +0100

    Add health endpoint

--- packages/api-service/src/app.ts
+++ packages/api-service/src/app.ts
@@ -8,6 +8,10 @@ app.get("/test", (req, res) => {
   res.json({ message: "test" });
 });
 
+app.get("/health", (req, res) => {
+  res.send("Account service is OK");
+});
+
 app.get("/readiness", (req, res) => {
   res.status(200).send("OK");
 });
