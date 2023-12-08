2a28b807260ca527250a767e9883e1c22a717831
Anders <>
2023-12-08 23:58:19 +0100

    peering comments

--- packages/service-types/src/user.ts
+++ packages/service-types/src/user.ts
@@ -1,16 +1,8 @@
 import { BaseEntity } from "./base-entity";
-import { Commander } from './commander';
-import { Gear } from './gear';
 
 export type UserRole = "admin" | "user";
 
 export interface ApiUser extends BaseEntity {
   email: string;
   role: UserRole;
-  loadouts: ApiUserCommanderLoadout[];
-}
-
-export interface ApiUserCommanderLoadout extends BaseEntity {
-  commander: Commander;
-  gear: Gear;
 }
