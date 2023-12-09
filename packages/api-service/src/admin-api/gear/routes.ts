a1f80b75670ad45a00ba132aff0129d73fa4fc11
Anders <>
2023-12-10 00:27:14 +0100

    create and get gear

--- packages/api-service/src/admin-api/gear/routes.ts
+++ packages/api-service/src/admin-api/gear/routes.ts
@@ -2,5 +2,6 @@ import { Router } from "express";
 import * as controller from "./controller";
 
 export const initGearRoutes = (router: Router) => {
-  router.route("/gear").get(controller.getAllGear).post(controller.createGear);
+  router.route("/gear").get(controller.getGear).post(controller.createGear);
+  router.route("/gearskill").get(controller.getGearSkills).post(controller.createGearSkill);
 };
