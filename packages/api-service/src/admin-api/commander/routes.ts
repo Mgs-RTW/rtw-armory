6b76add4e8aa17e9f4bf3edd09a749dd3f4c9afb
Anders <>
2023-12-13 15:02:06 +0100

    fix

--- packages/api-service/src/admin-api/commander/routes.ts
+++ packages/api-service/src/admin-api/commander/routes.ts
@@ -3,4 +3,5 @@ import * as controller from "./controller";
 
 export const initCommanderRoutes = (router: Router) => {
   router.route("/commander").post(controller.create);
+  router.route("/commander/assets").post(controller.commanderAsset);
 };
--- packages/api-service/src/api/commander/service.ts
+++ packages/api-service/src/api/commander/service.ts
@@ -3,19 +3,22 @@ import { sql } from "../../db";
 
 export async function getAllCommanders() {
   const commanders = await sql<Commander[]>`
-        SELECT 
-        c.id, 
-        c.name, 
-        image, 
-        tier, 
-        alignment, 
-        cr.id as raceId,
-        to_json(ca) as "baseData"
-        FROM commander c 
-        JOIN race cr on cr.id = c.race_id
-        LEFT JOIN commander_attributes ca
-        on ca.commander_id = c.id
-        group by c.id,cr.id,ca.*;
+          SELECT 
+          c.id, 
+          c.name, 
+          image, 
+          tier, 
+          alignment, 
+          cr.id as raceId,
+          to_json(ca) as "baseData",
+          to_json(cass) as "assets"
+          FROM commander c 
+          JOIN race cr on cr.id = c.race_id
+          LEFT JOIN commander_attributes ca
+          on ca.commander_id = c.id
+          LEFT JOIN commander_assets cass 
+          on cass.commander_id = c.id
+          group by c.id,cr.id,ca.*,cass.*;
     `;
   return commanders;
 }
