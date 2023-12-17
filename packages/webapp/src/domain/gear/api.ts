393006f61ce9022f5357fd30a6e94d04329e9bd4
Kevin Nemec <kevin.nemec@sony.com>
2023-12-17 21:22:30 +0100

    Fix type error

--- packages/webapp/src/domain/gear/api.ts
+++ packages/webapp/src/domain/gear/api.ts
@@ -1,6 +1,6 @@
 import { client } from "@/util";
-import { Gear, GearSlot } from "@lotr-rtw/service-types";
+import { ApiGear, GearSlot } from "@lotr-rtw/service-types";
 
 export function fetchGearBySlot(raceId: string, slot: GearSlot) {
-  return client.get<Gear[]>(`/gear/${raceId}/${slot}`);
+  return client.get<ApiGear[]>(`/gear/${raceId}/${slot}`);
 }
