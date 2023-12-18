0b0225e93ef127e0c98e32e9cc63d633ce583f3a
Anders <>
2023-12-18 21:33:15 +0100

    testing

--- packages/api-service/src/app.ts
+++ packages/api-service/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"
 import adminRouter from "./admin-api";
 import apiRouter from "./api";
 import {
@@ -8,8 +9,10 @@ import {
   sessionMiddleware,
 } from "./middlewares";
 import { extractAvailableEndpointsFromRouters } from "./util/express";
+import { corsOptions } from './util/get-cors-options';
 
 const app = express();
+app.use(cors(corsOptions()))
 app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -45,3 +48,5 @@ app.get("/readiness", (req, res) => {
 app.use(errorMiddleware);
 
 export default app;
+
+
