7a629843e53058d08c86b2d100c7976053e9f2eb
Kevin Nemec <kevin.nemec94@gmail.com>
2023-12-18 23:02:43 +0100

    Fix/cors (#41)
    Fix cors

--- packages/api-service/src/app.ts
+++ packages/api-service/src/app.ts
@@ -1,18 +1,18 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import adminRouter from "./admin-api";
 import apiRouter from "./api";
 import {
   authMiddleware,
+  corsMiddleware,
   errorMiddleware,
   requiresRoleMiddleware,
   sessionMiddleware,
 } from "./middlewares";
 import { extractAvailableEndpointsFromRouters } from "./util/express";
-import { corsOptions } from './util/get-cors-options';
 
 const app = express();
-app.use(cors(corsOptions()))
+app.use(corsMiddleware());
 app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -48,5 +48,3 @@ app.get("/readiness", (req, res) => {
 app.use(errorMiddleware);
 
 export default app;
-
-
