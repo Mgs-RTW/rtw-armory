8769d5cf6ef33e4e03d23dee004057753c578ca7
Kevin Nemec <kevin.nemec@sony.com>
2023-12-18 22:59:30 +0100

    fix

--- packages/api-service/src/app.ts
+++ packages/api-service/src/app.ts
@@ -4,15 +4,15 @@ import adminRouter from "./admin-api";
 import apiRouter from "./api";
 import {
   authMiddleware,
+  corsMiddleware,
   errorMiddleware,
   requiresRoleMiddleware,
   sessionMiddleware,
 } from "./middlewares";
 import { extractAvailableEndpointsFromRouters } from "./util/express";
-import { corsMiddleware } from "./middlewares/cors-middleware";
 
 const app = express();
-app.use(corsMiddleware);
+app.use(corsMiddleware());
 app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
