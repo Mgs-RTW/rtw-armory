5730b9cb268dcba4e2b8035b8bd37081d16480e6
Mgs <akerberganders@gmail.com>
2024-01-21 13:08:02 +0100

    Merged main and added default servers to pgadmin

--- packages/api-service/src/admin-api/loadout/routes.ts
+++ packages/api-service/src/admin-api/loadout/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import * as controller from "./controller";
 
-export const initGearRoutes = (router: Router) => {
+export const initLoadoutRoutes = (router: Router) => {
   router.route("/loadout").post(controller.createLoadout)
 };
